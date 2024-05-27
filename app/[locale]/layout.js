efdf4beb18233924e9bf2599fa62610c28018407
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-05-27 22:26:32 +0300

    test

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -26,7 +26,7 @@ const MainLayout = async ({ params: { locale }, children }) => {
   const { t, resources } = await initTranslations(locale, ['main', 'start']);
   const headersList = headers()
   const referer = headersList.get('referer')
-  console.log(process.env.NEXT_PRODUCTION,  referer.split('/')[2])
+
   return (
     <TranslationsProvider
       locale={locale}
