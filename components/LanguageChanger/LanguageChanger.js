6f5208cd21721a59ea75369239986c56a4aadb32
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-06-10 17:47:19 +0300

    test

--- components/LanguageChanger/LanguageChanger.js
+++ components/LanguageChanger/LanguageChanger.js
@@ -73,6 +73,30 @@ export default function LanguageChanger({ display, dividerClass }) {
       >
         UA
       </button>
+      <Script
+        src="https://cdn.cookiehub.eu/c2/ddd5f08e.js"
+        strategy="beforeInteractive"
+      />
+      <Script id="cookie-consent" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('consent','default', {
+            'security_storage': 'granted',
+            'functionality_storage': 'denied',
+            'personalization_storage': 'denied',
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied',
+            'wait_for_update': 500
+          });
+          document.addEventListener("DOMContentLoaded", function(event) {
+            var cpm = {};
+            window.cookiehub.load(cpm);
+          });
+        `}
+      </Script>
     </div>
   );
 }
