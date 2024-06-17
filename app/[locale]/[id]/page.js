c955bce2470bdce599ecb28512555d641832c31b
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-06-17 14:44:10 +0300

    test

--- app/[locale]/[id]/page.js
+++ app/[locale]/[id]/page.js
@@ -9,14 +9,14 @@ import initTranslations from "@/app/i18n";
 import TranslationsProvider from "@/app/TranslationsProvider";
 import NotFound from "../[...notFound]/page";
 
-export async function generateMetadata({ params: {locale, id} }) {
+/* export async function generateMetadata({ params: {locale, id} }) {
   const { t, resources } = await initTranslations(locale, ['main', 'start']);
   const product = productsArr(t).find(item => String(item.id) === id);
 
   return {
     title: `Uviten | ${product.title}`,
   }
-}
+} */
 
 const ProductPage = async ({ params: { locale, id } }) => {
     const { t, resources } = await initTranslations(locale, ['main', 'start']);
--- app/[locale]/projects/[id]/page.js
+++ app/[locale]/projects/[id]/page.js
@@ -9,14 +9,14 @@ import MoreProjects from "@/modules/ProductPage/MoreProjects/MoreProjects";
 import Contact from "@/modules/HomePage/Contact/Contact";
 import NotFound from "../../[...notFound]/page";
 
-export async function generateMetadata({ params: {locale, id} }) {
+/* export async function generateMetadata({ params: {locale, id} }) {
   const { t, resources } = await initTranslations(locale, ['main', 'start']);
   const product = productsArr(t).find(item => String(item.id) === id);
 
   return {
     title: `Uviten | ${product.title}`,
   }
-}
+} */
 
 const ProjectsSinglePage = async ({ params: { locale, id } }) => {
     const { t, resources } = await initTranslations(locale, ['main', 'start']);
