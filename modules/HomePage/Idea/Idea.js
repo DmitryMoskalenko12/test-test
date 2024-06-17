237cd1ce920c7dad4308e2c2915b562a5b6a216a
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-06-17 15:44:44 +0300

    test

--- modules/HomePage/Idea/Idea.js
+++ modules/HomePage/Idea/Idea.js
@@ -9,30 +9,7 @@ const Idea = async ({ locale }) => {
   const { t, resources } = await initTranslations(locale, ['start', 'main']);
 
   return (
-    <section className={classes.idea}>
-      <div className='container'>
-        <div className={classes.linesDesktop}>
-           <Image src={waves.src} width={2080} height={614} alt='You will see waves'/>
-        </div>
-        <div className={classes.lines}>
-           <Image src={wavesMobile.src} width={1021} height={604} alt='You will see waves'/>
-        </div>
-        <div className={classes.wrapper}>
-          <h1 className={classes.title}>
-            From idea to <span className={classes.delivery}>delivery</span>
-          </h1>
-
-          <div className={classes.potential}>
-            <p className={classes.text}>{t('ideaText')}</p>
-            <AnchorUI
-              clazz={classes.ideaButton}
-              href={'#contact'}
-              text={t('shedule')}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+<></>
   );
 };
 
